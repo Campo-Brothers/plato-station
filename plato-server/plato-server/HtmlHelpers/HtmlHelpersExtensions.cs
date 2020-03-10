90d4225ed3ce07e984e9e184e925f4de16317769
Luca campo Dall'Orto <luca.campodallorto@gmail.com>
2020-03-10 22:30:42 +0100

    Miglioramento card generate tramite HtmlHelpersExtensions

--- plato-server/plato-server/HtmlHelpers/HtmlHelpersExtensions.cs
+++ plato-server/plato-server/HtmlHelpers/HtmlHelpersExtensions.cs
@@ -5,16 +5,10 @@ namespace plato.server.HtmlHelpers
 {
     public static class HtmlHelpersExtensions
     {
-        public static IHtmlContent CreateSettingNavigationMenu(this IHtmlHelper html)
-        {
-            var footer = style + "" + menu;
-            
-
-            return new HtmlString(footer);
-        }
+        #region Private fields
 
         private static string style =
-            @"<style>
+           @"<style>
             ul.platoFooter {
               list-style-type: none;
               margin: 0;
@@ -50,5 +44,54 @@ namespace plato.server.HtmlHelpers
             "<li class=\"platoFooter\"><a class=\"platoFooter\" href=\"/Termo\"><img src=\"\\images\\thermSetting.png\" width=\"16\" height=\"16\"></a></li>" +
             "<li class=\"platoFooter\"><a class=\"platoFooter\" href=\"/SystemStat/SystemStat\"><img src=\"\\images\\system.png\" width=\"16\" height=\"16\"></a></li>" +
             "</ul>";
+
+        #endregion
+
+        public static IHtmlContent CreateSettingNavigationMenu(this IHtmlHelper html)
+        {
+            var footer = style + "" + menu;
+            return new HtmlString(footer);
+        }
+
+        public static IHtmlContent CreateDashboardCard(this IHtmlHelper html, string iconMain, string iconExtra, string value, string refValue, string link, WarningLevel level)
+        {
+            var card =
+                $" <div class=\"card {ConvertWarningLevel(level)}\" style=\"width: 15rem; \">                                 " +
+                 " 	<div class=\"card-body\">                                                            " +
+                 " 		<table>                                                                          " +
+                 " 			<tr>                                                                         " +
+                $" 				<td><img src=\"{iconMain}\" width=\"64\" height=\"64\"></td>             " +
+                $" 				<td><div class=\"display-3\">{value}</div></td>                          " +
+                 " 			</tr>                                                                        " +
+                 " 			<tr>                                                                         " +
+                $" 				<td><div class=\"display-4\">{refValue}</p></td>                                               " +
+                $" 				<td><img src=\"{iconExtra}\" width =\"24\" height=\"24\"></td>           " +
+                 " 			</tr>                                                                        " +
+                 " 		</table>                                                                         " +
+                $" 		<a href=\"{link}\" class=\"stretched-link\">Go</a>                               " +
+                 " 	</div>                                                                               " +
+                 " </div>                                                                                " ;
+            return new HtmlString(card);
+        }
+
+       
+
+        private static string ConvertWarningLevel(WarningLevel level)
+        {
+            switch(level)
+            {
+                case WarningLevel.bg_info:
+                    return "bg-info";
+                case WarningLevel.bg_light:
+                    return "bg-light";
+                case WarningLevel.bg_warning:
+                    return "bg-warning";
+                case WarningLevel.bg_danger:
+                    return "bg-danger";
+                default:
+                    return "bg-info";
+            }
+                
+        }
     }
 }
---
+++ plato-server/plato-server/HtmlHelpers/WarningLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace plato.server.HtmlHelpers
+{
+    public enum WarningLevel
+    {
+        bg_light,
+        bg_info,
+        bg_warning,
+        bg_danger
+    }
+}
